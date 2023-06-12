c1a0d12265fd2758110e711f6d25a2a00bfc4c76
Oskar Przybylski <oskar.przybylski23@gmail.com>
2023-06-12 21:22:15 +0100

    map changes

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 import TileMap from "./TileMap.js";
-import Character from "./character.js";
+// import Character from "./character.js";
 
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
@@ -7,17 +7,8 @@ const tileSize = 32;
 
 const tileMap = new TileMap(tileSize);
 
-// Create a character instance
-const character = new Character(100, 100, 32, 32, "./images/pacman");
-const startingPosition = { x: 100, y: 100 };
-
 function gameLoop() {
   tileMap.draw(canvas, ctx);
-    // Update the character
-    character.update();
-  
-    // Draw the character
-    character.draw(ctx);
   }
 
 setInterval(gameLoop, 1000 / 60);
